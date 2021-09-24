bd6598d18281a86e6f5d49a21ff79ff9c5804883
Bryan C Guner <bryan.guner@gmail.com>
2021-09-24 10:32:08 +0000

    hi

---
+++ script.js
@@ -0,0 +1,13 @@
+//-Start of Tawk.to Script-->
+
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/613041ecd6e7610a49b3326a/1feidq23q";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
